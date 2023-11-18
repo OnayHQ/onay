3008eeb4caeaf94bbb16f566ba33502005af3eee
Diogo Ferreira <diogomf@hey.com>
2023-11-18 21:43:52 +0300

    clean up next config and unused assets

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  webpack(config) {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
