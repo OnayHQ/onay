870963ef04fe55bbb707b96bb7e59e1861dce2ea
Berteotti <berteotti2@gmail.com>
2023-11-19 01:20:58 +0300

    feat: add networks

--- packages/app/context/Web3Modal.tsx
+++ packages/app/context/Web3Modal.tsx
@@ -14,7 +14,6 @@ const mainnet = {
   explorerUrl: "https://etherscan.io",
   rpcUrl: "https://cloudflare-eth.com",
 };
-
 const polygon = {
   chainId: 137,
   name: "Polygon",
@@ -23,6 +22,48 @@ const polygon = {
   rpcUrl: "https://polygon-rpc.com",
 };
 
+const arbitrum = {
+  chainId: 42161,
+  name: "Arbitrum",
+  currency: "ETH",
+  explorerUrl: "https://arbiscan.io/",
+  rpcUrl: "https://rpc.ankr.com/arbitrum",
+};
+const base = {
+  chainId: 8453,
+  name: "Base",
+  currency: "ETH",
+  explorerUrl: "https://basescan.org/",
+  rpcUrl: "https://base.meowrpc.com",
+};
+const celo = {
+  chainId: 42220,
+  name: "Celo",
+  currency: "CELO",
+  explorerUrl: "https://explorer.celo.org/mainnet/",
+  rpcUrl: "https://rpc.ankr.com/celo",
+};
+const gnosisChain = {
+  chainId: 100,
+  name: "Gnosis Chain",
+  currency: "xDAI",
+  explorerUrl: "https://gnosisscan.io/",
+  rpcUrl: "https://rpc.ankr.com/gnosis",
+};
+const polygonZkEvm = {
+  chainId: 1101,
+  name: "Polygon zkEVM",
+  currency: "ETH",
+  explorerUrl: "https://zkevm.polygonscan.com/",
+  rpcUrl: "https://polygon-zkevm.drpc.org",
+};
+const scroll = {
+  chainId: 534352,
+  name: "Scroll",
+  currency: "ETH",
+  explorerUrl: "https://scroll.io/rollupscan",
+  rpcUrl: "https://1rpc.io/scroll",
+};
 // 3. Create modal
 const metadata = {
   name: "Onay",
@@ -34,9 +75,9 @@ const metadata = {
 export const createModal = () =>
   createWeb3Modal({
     ethersConfig: defaultConfig({ metadata }),
-    chains: [polygon],
+    chains: [arbitrum, base, celo, gnosisChain, polygonZkEvm, scroll],
     projectId,
-    defaultChain: polygon,
+    defaultChain: arbitrum,
     themeMode: "light",
   });
 
