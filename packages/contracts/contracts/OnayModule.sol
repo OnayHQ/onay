48e6a204237c195b32d3c09cb56c3c21ee725312
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-11-18 08:52:25 +0300

    feat: bulk revoke

--- packages/contracts/contracts/OnayModule.sol
+++ packages/contracts/contracts/OnayModule.sol
@@ -5,18 +5,40 @@ import {Enum} from "./helpers/Enum.sol";
 import {ISafe} from "./interfaces/ISafe.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OnayModule is Ownable {
-    string public constant NAME = "Onay Module";
-    string public constant VERSION = "0.1.0";
+error ExecuteTokenRevokeFailed();
+error InvalidParameters();
 
-    constructor() Ownable(msg.sender) {}
+contract OnayModule is Ownable {
+  string public constant NAME = "Onay Module";
+  string public constant VERSION = "0.1.0";
+
+  constructor() Ownable(msg.sender) {}
     
-    function revokeAllowance(
-        ISafe safe,
-        address token,
-        address approvedContract
-    ) external onlyOwner {
-        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", approvedContract, 0);
-        require(safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token revoke");
+  function revokeAllowance(
+    ISafe safe,
+    address token,
+    address approvedContract
+  ) external onlyOwner {
+    bytes memory data = abi.encodeWithSignature("approve(address,uint256)", approvedContract, 0);
+    if (!safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call)) {
+      revert ExecuteTokenRevokeFailed();
     }
+  }
+
+  function revokeAllowanceBulk(
+    ISafe[] calldata safes,
+    address[] calldata tokens,
+    address[] calldata approvedContracts
+  ) external onlyOwner {
+    if (safes.length != tokens.length || tokens.length != approvedContracts.length) {
+      revert InvalidParameters();
+    }
+
+    for (uint i = 0; i < safes.length; i++) {
+      bytes memory data = abi.encodeWithSignature("approve(address,uint256)", approvedContracts[i], 0);
+      if (!safes[i].execTransactionFromModule(tokens[i], 0, data, Enum.Operation.Call)) {
+        revert ExecuteTokenRevokeFailed();
+      }
+    }
+  }
 }
