8f46844d74043ca4837e03baa56961c4e5d23b5a
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-11-18 05:01:58 +0300

    feat: deploy script

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.8.23",
+  networks: {
+    ethereum: {
+      url: process.env.ETHEREUM_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    gnosis: {
+      url: "https://rpc.ankr.com/gnosis",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    url: "",
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
--- packages/contracts/scripts/deploy.js
+++ packages/contracts/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const contract = await hre.ethers.deployContract("OnayModule");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await contract.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `OnayModule deployed to ${contract.target}`
   );
 }
 
